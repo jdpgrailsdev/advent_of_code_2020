0668e1c8651f438a1776ee6fd11f29e1a8c75c7b
Jonathan Pearlin <jpearlin@newrelic.com>
2020-12-11 12:50:11 -0500

    Fix compile errors

--- src/day_11.rs
+++ src/day_11.rs
@@ -2,49 +2,59 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub fn exercise() {
-    let data = load_data();
+    let mut data = load_data();
 	
-	let seating_chart = compute(&mut data);
+	compute(&mut data);
 }
 
 fn compute(data: &mut Vec<Vec<char>>) {
 	let mut current_row = 0;
 	let mut current_col = 0;
 	let mut changes = 0;
+	let mut updated_rows: Vec<Vec<char>> = data.clone();
+	let length = updated_rows.len();
 
-	for row in data.iter() {
+	for r in 0..length {
+		let mut updated_seats: Vec<char> = vec![];
+		let row = updated_rows.get(r).unwrap();
+		
 		for seat in row {
-			let open_seats = check_open_seats(data.to_vec(), current_row, current_col);		
-			println!("There are {} open seats around seat {} in row {}.", open_seats, current_col, current_row);
+			let open_seats = check_open_seats(updated_rows.to_vec(), current_row, current_col);		
+//			println!("There are {} open seats around seat {} in row {}.", open_seats, current_col, current_row);
 			if *seat == 'L' {
 				if open_seats == 8 {
-					data[current_row as usize][current_col as usize] = '#';
+					updated_seats.push('#');
 					changes += 1;
 				} else {
-					
+					updated_seats.push(*seat);
 				}
 			} else if *seat == '#' {
 				if open_seats <= 4 {
-					data[current_row as usize][current_col as usize] = 'L';
+					updated_seats.push('L');
 					changes += 1;
-				} 
+				} else {
+					updated_seats.push(*seat);
+				}
+			} else {
+				updated_seats.push(*seat);
 			}
 			
 			current_col += 1;
 		}
+		
+		updated_rows.push(updated_seats);
 		
 		current_row += 1;
 		current_col = 0;
 	}
 	
 	if changes > 0 {
-		compute(data);
+		compute(&mut updated_rows);
 	} else {
 		let mut occupied = 0;
-		for row in data {
+		for row in updated_rows {
 			for seat in row {
-				println!("Looing at seat {}", seat);
-				if *seat == '#' {
+				if seat == '#' {
 					occupied += 1;
 				}
 			}
